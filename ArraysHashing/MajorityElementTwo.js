0154f70d975833c9177f382b18fe2c30cea9f2c1
taolu-career <taolu.career@gmail.com>
2024-12-30 23:41:06 -0500

    leetcode

--- ArraysHashing/MajorityElementTwo.js
+++ ArraysHashing/MajorityElementTwo.js
@@ -3,15 +3,14 @@
  * @return {number[]}
  */
 var majorityElement = function (nums) {
-  let myMap = new Map();
-  let len = nums.length / 3;
+  let len = Math.floor(nums.length / 3);
   let res = [];
-
+  let map = new Map();
   for (let num of nums) {
-    myMap.set(num, (myMap.get(num) || 0) + 1);
+    map.set(num, (map.get(num) || 0) + 1);
   }
 
-  for (let [key, value] of myMap.entries()) {
+  for (let [key, value] of map) {
     if (value > len) {
       res.push(key);
     }
