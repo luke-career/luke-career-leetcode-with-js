bed74810d71689e3a103635d1039ffcfa692c376
taolu-career <taolu.career@gmail.com>
2025-01-12 22:42:01 -0500

    leetcode

--- ArraysHashing/TwoSum.js
+++ ArraysHashing/TwoSum.js
@@ -16,3 +16,21 @@ var twoSum = function (nums, target) {
   }
   return res;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let map = new Map();
+  let res = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      res.push(map.get(target - nums[i]));
+      res.push(i);
+    }
+    map.set(nums[i], i);
+  }
+  return res;
+};
